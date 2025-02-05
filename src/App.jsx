fc9fc5207c437ee53e8cf0f1366d33c16d7885b6
netsiri <gunrattanasuwan@gmail.com>
2025-02-05 16:55:47 +0700

    test change ui v2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       }}>
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
           <span style={{ fontSize: "24px", cursor: "pointer" }}>☰</span>
-          <span>For life</span>
+          <span>For Me</span>
         </div>
       </nav>
 
